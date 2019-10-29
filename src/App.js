b567bd2d6481dfd253531461ddfee5b256c04b75
gamer05 <gamer05@list.ru>
2019-10-29 16:44:55 +0300

    Размечена главная страница.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Поехали!</h1>
-      </div>
+      <Layout>
+        <Quiz />
+      </Layout>
     );
   }
 }
